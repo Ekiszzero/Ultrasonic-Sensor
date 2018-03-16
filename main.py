e76ceb1dbf5937075de7e0441c091bba065802ec
El Piero <Ekiszzero@users.noreply.github.com>
2018-03-15 22:07:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # Archivo principal MAIN
 import machine
 import time
+import sys
 from machine import Pin
+
 
 #trigger en D5
 trigger = Pin(14, mode=Pin.OUT, pull=None)
@@ -19,12 +21,27 @@ def sensor_values():
 	trigger.value(0)
 	# activar echo
 	pulso = machine.time_pulse_us(echo, 1, 30000)
-	# Calcular distancia
-	# Velocidad de sonido a 20°C a 0msnm 343,2 m/s
-	dist = pulso * 0.01716
+	dist = round(pulso * 0.01716, 2)
+	a=0
+	
+	while dist<2 or dist>400:
+			trigger.value(1)
+			time.sleep_us(10)
+			trigger.value(0)
+			pulso = machine.time_pulse_us(echo, 1, 30000)
+			dist = round(pulso * 0.01716, 2)
+			a+=1
+			if a==2:
+				sys.stdout.write('--> midiendo..')
+			elif a>2:
+				sys.stdout.write('.')
+
+	if a!=0:
+		print("")
+
 	print("Distancia: ", dist,"cm")
 
 # imprime valores cada segundo
 while True:
 	sensor_values()	
-	time.sleep(1)
+	time.sleep(2)
